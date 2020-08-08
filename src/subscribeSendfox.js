1016d4803b2dad0162cad91d4dd234eb5df6b7af
Jose Sotelo <jsotelocohen@gmail.com>
2020-08-07 22:26:40 -0500

    changed form params to body fo sendfox requests

--- src/subscribeSendfox.js
+++ src/subscribeSendfox.js
@@ -1,17 +1,24 @@
 export function addToSendfox(email, list, name, lastname) {
 
-  const encodedUrl = `/sendfox/contacts?email=${email}&lists[]=${list.list}`;
+  const encodedUrl = 'https://api.sendfox.com/contacts';
+  //const encodedUrl = '/sendfox-contact/';
 
   const headers = new Headers();
   headers.append('Authorization', `Bearer ${process.env.GATSBY_SENDFOX_API_TOKEN}`);
 
+  const data = {
+    email: email,
+    'list': list.list
+  }
   const config = {
     method: 'POST',
     headers: headers,
     mode: 'cors',
     cache: 'default',
-    redirect:'error'
+    redirect:'error',
+    body: JSON.stringify(data)
   }
+
 
   return fetch(encodedUrl, config)
     .then(result => {
