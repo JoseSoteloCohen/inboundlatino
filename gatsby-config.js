358ddb60b7c9740d60527980b74f1fcbe66b47be
Jose Sotelo <jsotelocohen@gmail.com>
2020-08-08 23:48:38 -0500

    Implemented Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,5 +62,28 @@ module.exports = {
         legacy: false
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-WXJK6G",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: "gatsby-route-change",
+      },
+    },
   ],
 }
